51d039c63fe4ccc2b341931df321183ac77ba39d
Sergio Kayap <cristhiankayap163@gmail.com>
2025-07-20 13:46:53 -0500

    modificamos conexion bd

--- AccesoCiudadano/settings.py
+++ AccesoCiudadano/settings.py
@@ -69,11 +69,9 @@ WSGI_APPLICATION = 'AccesoCiudadano.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/bd-acceso-ciudadano',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 
 # Password validation
