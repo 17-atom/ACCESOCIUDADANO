35e8848319b7bc410d5224a1d226a014848bc24a
Sergio Kayap <cristhiankayap163@gmail.com>
2025-07-20 14:09:39 -0500

    MOdificando static root

--- AccesoCiudadano/settings.py
+++ AccesoCiudadano/settings.py
@@ -114,11 +114,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
